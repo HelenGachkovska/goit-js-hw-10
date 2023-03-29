65ae44ce3da08f170737c8f81034d0b15da25a41
HelenGachkovska <ogachkovska@gmail.com>
2023-03-29 19:24:32 +0300

    Готова робота

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import debounce from 'lodash.debounce';
+import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
@@ -7,56 +10,81 @@ const countriInfo = document.querySelector('.country-info');
 
 let nameCountry = '';
 
-inputEl.addEventListener('input', hendlerInputCountry);
+// При Інпуті чистимо данні і викликаємо обробник події
+
+inputEl.addEventListener(
+  'input',
+  debounce(ev => {
+    countryList.innerHTML = '';
+    countriInfo.innerHTML = '';
+    hendlerInputCountry(ev);
+  }, DEBOUNCE_DELAY)
+);
+
+// Обробляємо проміс
 
 function hendlerInputCountry(ev) {
   nameCountry = ev.target.value;
-  console.log(nameCountry);
-  fetchCountries(nameCountry);
+  if (nameCountry !== '') {
+    fetchCountries(nameCountry)
+      .then(data => {
+        verifyResponse(data);
+        return data;
+      })
+      .catch(error => {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      });
+  }
 }
 
-function fetchCountries(name) {
-  return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
-  )
-    .then(response => response.json())
-    .then(data => {
-      console.log(data.length);
-      verifyResponse(data);
-      return data;
-    });
-}
+// Перевіряємо данні від проміса
 
 function verifyResponse(data) {
   if (data.length > 10) {
-    // alert('забагато хочеш');
-    // return;
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    return;
   }
   if (data.length === 1) {
-    console.log('одна-єдина');
-    createListCountry(data);
+    createCountry(data);
     return;
   } else {
-    console.log('список країн');
+    createListCountry(data);
   }
 }
 
-function createListCountry(data) {
-    console.log(data);
-    const [
-        {
-            name: { official },
-            capital,
-            population,
-            flags: { svg, alt },
-            languages,
+// Створюємо країну(одна)
+
+function createCountry(data) {
+  const [
+    {
+      name: { official },
+      capital,
+      population,
+      flags: { svg, alt },
+      languages,
     },
-    ] = data;
-    console.log(...languages);
+  ] = data;
+
+  const lang = Object.values(languages);
 
   countriInfo.innerHTML = `<img src=${svg} alt=${alt} width="50" height="50">
 <h1>${official}</h1>
-<p>Capital: ${capital}</p>
-<p>Population: ${population}</p>
-<p>Languages: ${languages}</p>`;
+<p><span class="title">Capital:</span> ${capital}</p>
+<p><span class="title">Population:</span> ${population}</p>
+<p><span class="title">Languages:</span> ${lang.join(', ')}</p>`;
+}
+
+// Створюємо список країн
+
+function createListCountry(data) {
+  data.forEach(element => {
+    const countryListEl = `<li>
+  <img src=${element.flags.svg} alt=${element.flags.alt} width="50" height="50">
+  <p>${element.name.official}</p>
+  </li>`;
+
+    countryList.insertAdjacentHTML('beforeend', countryListEl);
+  });
 }
